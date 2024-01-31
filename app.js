e6897de09691128ae579affb7f3983dd59fb2726
Valentin <vpavonlopez@gmail.com>
2024-01-31 16:33:02 +0100

    principio crud para clase con json

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express')
+
+const app =  express()
+
+const morgan = require('morgan')
+
+
+app.use(morgan('dev'))
+
+app.use(express.urlencoded({extended:false}))
+app.use(express.json())
+
+app.use('/',require('./routes/main'))
+
+
+let port = process.env.PORT || 3000
+app.listen(port, (req,res)=>{
+    console.log(`server running on port ${port}`);
+})
---
+++ routes/main.js
@@ -0,0 +1,11 @@
+const express = require('express')
+
+const router = express.Router()
+
+router.get('/',(req,res)=>{
+    resizeBy.send('hola mundo')
+})
+
+
+
+module.exports = router
